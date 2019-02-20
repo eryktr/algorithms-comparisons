433c2ee89056d56191dc137a9d31f63e0cec8a2d
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-02-21 00:36:39 +0100

    Adddd fibonacci algorithm working in O(log(n)) time and its tests.

--- algorithms_comparisons/fibonacci/compare.py
+++ algorithms_comparisons/fibonacci/compare.py
@@ -2,8 +2,8 @@ from algorithms_comparisons.analysis.compare import compare
 from algorithms_comparisons.fibonacci import fibonacci
 
 def compare_fibonacci():
-    functions = [fibonacci.dynamic_fibonacci, fibonacci.recursive_fibonacci]
-    params = [i for i in range(1, 10)]
+    functions = [fibonacci.dynamic_fibonacci, fibonacci.matrix_fibonacci, fibonacci.recursive_fibonacci]
+    params = [i for i in range(1, 20)]
     compare(functions, params)
 
 if __name__ == "__main__":
--- algorithms_comparisons/fibonacci/fibonacci.py
+++ algorithms_comparisons/fibonacci/fibonacci.py
@@ -1,11 +1,43 @@
+import numpy as np
 def recursive_fibonacci(n):
-    return n - 1 if n == 1 or n == 2 else recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
+    return n  if n == 0 or n == 1 else recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
 
 def dynamic_fibonacci(n):
-    size = n+2
+    if n == 0 or n == 1:
+        return n
+    size = n+1
     memoized = [0] * size
-    memoized[2] = 1
-    for i in range(3,size):
+    memoized[1] = 1
+    for i in range(2,size):
         memoized[i] = memoized[i-1] + memoized[i-2]
     return memoized[n]
 
+def matrix_fibonacci(n):
+    Q = np.array([[1,1],[1,0]])
+    cache = {}
+
+    def get_matrix_power(matrix, power):
+        if power == 1:
+            return matrix
+        if power in cache:
+            return cache[power]
+        tmp = get_matrix_power(matrix, int(power/2))
+        result = tmp @ tmp
+        cache[power] = result
+        return result
+
+    def get_fibonacci_number(n):
+        if n == 0 or n == 1:
+            return n
+        else: 
+            powers = [2**power for (power, digit) in enumerate(reversed(bin(n-1)[2:])) if digit == '1']
+            matrices = [get_matrix_power(Q, power) for power in powers]
+
+            while len(matrices) > 1:
+                M1 = matrices.pop()
+                M2 = matrices.pop()
+                res = M1 @ M2
+                matrices.append(res)
+            return matrices[0][0][0]
+
+    return get_fibonacci_number(n)
